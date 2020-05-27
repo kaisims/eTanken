02f314b729faedd6a1d4f6d653b8dc7ee89be083
Kai Röschinger <kroeschi@th-koeln.de>
2020-05-27 20:36:31 +0200

    UI Update - Add AGB, Privacy and Bon outpu

--- app/js/authorise.js
+++ app/js/authorise.js
@@ -2,6 +2,7 @@ function loadUrl(location) {
     console.log(location);
     window.location = location;
 }
+
 function authorise(){
     var ajax = new XMLHttpRequest();
     ajax.onreadystatechange = function () {
@@ -11,8 +12,44 @@ function authorise(){
             console.log(link)
             loadUrl(link);
         }
+        if (ajax.readyState== 4 && ajax.status!=200){
+            location.reload()
+        }
     }
     ajax.open("OPTIONS", "");
     ajax.send();
 }
+
+function showAgb(){
+    modal.setContent("Wird geladen..")
+    var ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState==4 && ajax.status==200) {
+            modal.setContent('<h1>Allgemeine Geschäftsbedingungen</h1> '+ this.responseText)
+        }
+    }
+    ajax.open("GET", "/getAgb/");
+    ajax.send();
+    modal.open()
+}
+
+function showPP() {
+    modal.setContent("Wird geladen..")
+    var ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState==4 && ajax.status==200) {
+            modal.setContent('<h1>Privacy Policy</h1> '+ this.responseText)
+        }
+    }
+    ajax.open("GET", "/getPP/");
+    ajax.send();
+    modal.open()
+}
 authorise()
+
+var modal = new tingle.modal({
+    footer: false,
+    stickyFooter: false,
+    closeMethods: ['overlay', 'button', 'escape'],
+    closeLabel: "Close",
+});
